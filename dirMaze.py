9a28f6acc3448cf694b02b9ac332649d0db4e722
Patrick <revmydev@gmail.com>
2019-06-05 15:48:52 -0700

    added license stuff

---
+++ dirMaze.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Patrick Strube
+
+# directoryMaze is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+
+# directoryMaze is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+# This software is provided AS-IS with no warranty.
+# You should have received a copy of the GNU Public License V3 with this software, but if you didn't,
+# you can find it here: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+
