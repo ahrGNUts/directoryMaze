a1599861dd1e4deec0f22bdf7c3696f9a9c0dca2
Patrick <revmydev@gmail.com>
2019-06-05 16:27:07 -0700

    skeleton for script init, builder class

---
+++ MazeBuilder.py
@@ -0,0 +1,8 @@
+class MazeBuilder:
+
+
+    def __init__(self):
+        pass
+
+    def build(self):
+        pass
--- dirMaze.py
+++ dirMaze.py
@@ -13,4 +13,24 @@
 # You should have received a copy of the GNU Public License V3 with this software, but if you didn't,
 # you can find it here: https://www.gnu.org/licenses/gpl-3.0.en.html
 
+import MazeBuilder
 
+if __name__ == '__main__':
+    to_hide_path = input("Enter the path to the file or directory you want to obfuscate: ")
+    maze_root = input("Enter the path where you want the maze to start: ")
+    root_name = input("Enter the name of the root folder node (Leave blank for random): ")
+    layer_str = input("How many layers deep should the maze be? (Enter a number; max 20): ")
+
+    try:
+        val = int(layer_str)
+        if val == 0 or val > 20:
+            raise ValueError
+    except ValueError:
+        while not layer_str.isnumeric() or (layer_str.isnumeric() and (int(layer_str) < 1 or int(layer_str) > 20)):
+            print("Invalid depth.")
+            layer_str = input("How many layers deep should the maze be? (Enter a number; max 20): ")
+
+    layers = int(layer_str)
+
+    builder = MazeBuilder.MazeBuilder()
+    builder.build()
