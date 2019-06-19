99afe8d714b3bca96c1ad462d13ecd34210572cb
Patrick <revmydev@gmail.com>
2019-06-19 12:25:28 -0700

    more descriptive name for maze_root_path var

--- MazeBuilder.py
+++ MazeBuilder.py
@@ -1,8 +1,8 @@
 class MazeBuilder:
 
-    def __init__(self, to_hide_path, maze_root, root_node_name, layers, num_branches):
+    def __init__(self, to_hide_path, maze_root_path, root_node_name, layers, num_branches):
         self.to_hide_path = to_hide_path
-        self.maze_root = maze_root
+        self.maze_root_path = maze_root_path
         self.root_node_name = root_node_name
         self.layers = layers
         self.num_branches = num_branches
