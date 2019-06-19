61cc1157ad532c5371cb190eb40e15aaaf990971
Patrick <revmydev@gmail.com>
2019-06-19 13:05:23 -0700

    set root node name method + private generate dir name methd

--- MazeBuilder.py
+++ MazeBuilder.py
@@ -1,10 +1,14 @@
 import os
+import random
+import string
+
+
 class MazeBuilder:
 
     def __init__(self, to_hide_path, maze_root_path, root_node_name, layers, num_branches):
         self.to_hide_path = to_hide_path
-        self.root_node_name = root_node_name
         self.maze_root_path = self.__set_root_path(maze_root_path)
+        self.root_node_name = self.__set_root_node_name(root_node_name)
         self.layers = layers
         self.num_branches = num_branches
 
@@ -16,3 +20,13 @@ class MazeBuilder:
             return os.getcwd()
 
         return maze_root_path
+
+    def __set_root_node_name(self, root_node_name):
+        if root_node_name == "":
+            return self.__generate_dir_name(7)
+
+        return root_node_name
+
+    def __generate_dir_name(self, name_length):
+        char_pool = string.ascii_lowercase + string.digits
+        return ''.join(random.choice(char_pool) for i in range(name_length))
